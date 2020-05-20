d5cf994dca20a6439c7d2068e0c76aec10fb665d
Sebastián Iñones <sebainones@gmail.com>
2020-05-20 21:39:50 +0200

    Minor refacotring

--- DotNetCoreAngularAPp/Program.cs
+++ DotNetCoreAngularAPp/Program.cs
@@ -14,38 +14,53 @@ namespace DotNetCoreAngularApp
         public static void Main(string[] args)
         {
 
-            IConfigurationRoot config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddEnvironmentVariables()
-            .AddJsonFile("certificate.json", optional: true, reloadOnChange: true)
-            .AddJsonFile($"certificate.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
-            .Build();
+            try
+            {
+                IConfigurationRoot config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddEnvironmentVariables()
+               .AddJsonFile("certificate.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"certificate.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+               .Build();
 
+                string dbPassword = config.GetValue<string>("dbPwd");
+
+                X509Certificate2 certificate = GenrateCertificate(config);
+
+                CreateWebHostBuilder(args)
+                //TODO: If i Uncomment this, then the Angular Client does not launch automatically and it NO longer works!!!
+                //Need to sort this out!!!
+                //.UseKestrel(
+                //    options =>
+                //    {
+                //        options.AddServerHeader = false;
+                //        options.ListenLocalhost(5001, listenOptions =>
+                //        {
+                //            listenOptions.UseHttps(certificate);
+                //        });
+                //    })
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .Build()
+                .Run();
+            }
+            //TODO: handle this properly!
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private static X509Certificate2 GenrateCertificate(IConfigurationRoot config)
+        {
+            X509Certificate2 certificate;
+            //TODO: Re-generate the certificate AGAIN!
             var certificateSettings = config.GetSection("certificateSettings");
             string certificateFileName = certificateSettings.GetValue<string>("filename");
             string certificatePassword = certificateSettings.GetValue<string>("password");
-
-            string dbPassword = config.GetValue<string>("dbPwd");
-
-            var certificate = new X509Certificate2(certificateFileName, certificatePassword);
-
-
-            CreateWebHostBuilder(args)
-            //TODO: If i Uncomment this, then the Angular Client does not launch automatically and it NO longer works!!!
-            //Need to sort this out!!!
-            //.UseKestrel(
-            //    options =>
-            //    {
-            //        options.AddServerHeader = false;
-            //        options.ListenLocalhost(5001, listenOptions =>
-            //        {
-            //            listenOptions.UseHttps(certificate);
-            //        });
-            //    })
-            .UseConfiguration(config)
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .Build()
-            .Run();
+            certificate = new X509Certificate2(certificateFileName, certificatePassword);
+            return certificate;
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
